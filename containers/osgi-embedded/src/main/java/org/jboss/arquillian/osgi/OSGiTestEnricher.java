b47ebd6e8d38180f667ff8048ef4ac73bb15e41c
Thomas Diesler <thomas.diesler@jboss.com>
2010-06-24 08:43:57 +0000

    osgi: Add osgi container and packager

---
+++ containers/osgi-embedded/src/main/java/org/jboss/arquillian/osgi/OSGiTestEnricher.java
@@ -0,0 +1,42 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2009, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.arquillian.osgi;
+
+import java.lang.reflect.Method;
+
+import org.jboss.arquillian.spi.Context;
+import org.jboss.arquillian.spi.TestEnricher;
+
+/**
+ * [TODO] OSGiTestEnricher
+ *
+ * @author Thomas.Diesler@jboss.com
+ * @version $Revision: $
+ */
+public class OSGiTestEnricher implements TestEnricher
+{
+   @Override
+   public void enrich(Context context, Object testCase)
+   {
+   }
+
+   @Override
+   public Object[] resolve(Context context, Method method)
+   {
+      return null;
+   }
+}
