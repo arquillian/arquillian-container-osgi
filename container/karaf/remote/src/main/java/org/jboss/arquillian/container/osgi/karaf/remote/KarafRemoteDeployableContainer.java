f3a41977369962a6117a6d7b27fbcff9bfe00956
Tim Whittington <Tim.Whittington@orionhealth.com>
2014-09-26 21:09:55 +1200

    Fail Karaf remote deployable container startup if connection to JMX times out.
    Previously would log and fail with a NPE.

--- container/karaf/remote/src/main/java/org/jboss/arquillian/container/osgi/karaf/remote/KarafRemoteDeployableContainer.java
+++ container/karaf/remote/src/main/java/org/jboss/arquillian/container/osgi/karaf/remote/KarafRemoteDeployableContainer.java
@@ -71,7 +71,7 @@ public class KarafRemoteDeployableContainer<T extends KarafRemoteContainerConfig
             mbeanServer = getMBeanServerConnection(30, TimeUnit.SECONDS);
             mbeanServerInstance.set(mbeanServer);
         } catch (TimeoutException e) {
-            _logger.error("Error connecting to Karaf MBeanServer: ", e);
+            throw new LifecycleException("Error connecting to Karaf MBeanServer: ", e);
         }
 
         try {
